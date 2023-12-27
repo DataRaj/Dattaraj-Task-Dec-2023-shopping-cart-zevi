9b3ab1658ee601f7c0e7bcd1b9bc3dd7218919a2
Dattaraj Dhadas <dhadas808@yahoo.com>
2023-12-28 01:52:08 +0530

    added faker

--- src/fakeapi/FakeApi.tsx
+++ src/fakeapi/FakeApi.tsx
@@ -28,3 +28,38 @@ export function fetchSmartData(){
     }
     return smartData;
 }
+
+export function FetchProducts(){
+    const products: ProductProps[] = [];
+  
+    for (let i = 0; i <= 30; i++) {
+      const productImg = faker.image.url({
+        height: 400,
+        width: 300,
+      });
+      const productName = faker.vehicle.bicycle();
+      const productRating = faker.number.int({ min: 1, max: 5 });
+      const productOrgPrice = Number(
+        faker.commerce.price({ min: 300, max: 5000 })
+      );
+      const productDisPrice = Number(
+        faker.commerce.price({
+          min: 300,
+          max: Number(productOrgPrice),
+        })
+      );
+      const productsReviews = faker.number.int({ min: 10, max: 50 });
+  
+      products.push({
+        productImg,
+        productName,
+        productRating,
+        productOrgPrice,
+        productDisPrice,
+        productsReviews,
+      });
+    }
+  
+    return products;
+  };
+  
